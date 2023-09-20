14b42e5a3c3fc629dd452c028c240a2203746828
Nelson Torres Perlaza <nelson.torresp@externos.bci.cl>
2023-09-20 09:12:53 -0500

    flatMap

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -18,19 +23,73 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// ejemploIterable();
+		ejemploFlatMap();
+	}
 
 
-		Flux<String> nombres = Flux.just("Uno", "Dos","Tres", "", "Cuatro")
-				.doOnNext( e -> {
-					if( e.isEmpty()) {
+	public void ejemploFlatMap() throws Exception {
+
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("Andres Guzman");
+		usuariosList.add("Pedro Ramirez");
+		usuariosList.add("Camilo Vargas");
+		usuariosList.add("Juan Gomez");
+		usuariosList.add("Cristian Torres");
+		usuariosList.add("Nelson Torres");
+
+		Flux.fromIterable(usuariosList)
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.flatMap(usuario -> {
+					if( usuario.getNombre().equalsIgnoreCase("andres")) {
+						return Mono.just(usuario);
+					} else {
+						return  Mono.empty();
+					}
+				})
+
+				.map( usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				}).subscribe(e-> log.info(e.toString())
+		);
+
+	}
+
+
+	public void ejemploIterable() throws Exception {
+
+
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("Andres Guzman");
+		usuariosList.add("Pedro Ramirez");
+		usuariosList.add("Camilo Vargas");
+		usuariosList.add("Juan Gomez");
+		usuariosList.add("Cristian Torres");
+		usuariosList.add("Nelson Torres");
+
+
+		Flux<String> nombres = Flux.fromIterable(usuariosList);
+
+
+		Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("andres") )
+				.doOnNext( usuario -> {
+					if( usuario == null) {
 						throw new RuntimeException("Nombres no pueden ser vacios");
 					}
-					System.out.println(e);
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
 
+				})
+				.map( usuario -> {
+					String nombre = usuario.getNombre().toUpperCase();
+					usuario.setNombre(nombre);
+					return usuario;
 				});
 
 
-		nombres.subscribe(log::info,
+		usuarios.subscribe(e-> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					@Override
